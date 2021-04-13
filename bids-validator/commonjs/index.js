b56190b3d3c7b16b9ce3d4b49a9635d8b42ad525
David Nishikawa <david@squishymedia.com>
2021-04-13 12:44:10 -0700

    Separate TextDecoder.

--- bids-validator/commonjs/index.js
+++ bids-validator/commonjs/index.js
@@ -1,5 +1,11 @@
 // import validations
 import validate from '../validators'
 
+// Work around JSDom not providing TextDecoder yet
+if (typeof TextDecoder === 'undefined') {
+  const { TextDecoder } = require('util')
+  global.TextDecoder = TextDecoder
+}
+
 // export validations for use in other applications
 module.exports = validate

e7bd0dff75487d94767c208bea60f1b127227ca9
Etienne Bergeron <etienne-1.bergeron@polymtl.ca>
2021-07-28 14:21:34 -0400

    Added the microscopy Regex parts in the rules and added the function IsMicroscopy called by IsBids in type.js

--- bids-validator/utils/type.js
+++ bids-validator/utils/type.js
@@ -59,6 +59,7 @@ const megCrosstalkData = buildRegExp(file_level_rules.meg_crosstalk)
 const stimuliData = buildRegExp(file_level_rules.stimuli)
 const petData = buildRegExp(file_level_rules.pet)
 const petBlood = buildRegExp(file_level_rules.pet_blood)
+const microscopyData = buildRegExp(file_level_rules.microscopy)
 // Phenotypic data
 const phenotypicData = buildRegExp(phenotypic_rules.phenotypic_data)
 // Session level
@@ -71,6 +72,7 @@ const ieegSes = buildRegExp(session_level_rules.ieeg_ses)
 const megSes = buildRegExp(session_level_rules.meg_ses)
 const scansSes = buildRegExp(session_level_rules.scans)
 const petSes = buildRegExp(session_level_rules.pet_ses)
+const microscopySes = buildRegExp(session_level_rules.microscopy_ses)
 // Subject level
 const subjectLevel = buildRegExp(subject_level_rules.subject_level)
 // Top level
@@ -85,6 +87,7 @@ const multiDirFieldmap = buildRegExp(top_level_rules.multi_dir_fieldmap)
 const otherTopFiles = buildRegExp(top_level_rules.other_top_files)
 const megTop = buildRegExp(top_level_rules.meg_top)
 const petTop = buildRegExp(top_level_rules.pet_top)
+const microscopyTop = buildRegExp(top_level_rules.microscopy_top)
 
 export default {
   /**
@@ -110,7 +113,8 @@ export default {
       this.file.isFieldMap(path) ||
       this.file.isPhenotypic(path) ||
       this.file.isPET(path) ||
-      this.file.isPETBlood(path)
+      this.file.isPETBlood(path) ||
+      this.file.isMicroscopy(path)
     )
   },
 
@@ -134,7 +138,8 @@ export default {
           megTop.test(path) ||
           eegTop.test(path) ||
           ieegTop.test(path) ||
-          petTop.test(path)
+          petTop.test(path) ||
+          microscopyTop.test(path)
         )
       } else {
         return (
@@ -148,7 +153,8 @@ export default {
           megTop.test(path) ||
           eegTop.test(path) ||
           ieegTop.test(path) ||
-          petTop.test(path)
+          petTop.test(path) ||
+          microscopyTop.test(path)
         )
       }
     },
@@ -203,7 +209,8 @@ export default {
         conditionalMatch(megSes, path) ||
         conditionalMatch(eegSes, path) ||
         conditionalMatch(ieegSes, path) ||
-        conditionalMatch(petSes, path)
+        conditionalMatch(petSes, path) ||
+        conditionalMatch(microscopySes, path)
       )
     },
 
@@ -339,6 +346,10 @@ export default {
       }
     },
 
+    isMicroscopy: function(path) {
+      return conditionalMatch(microscopyData, path)
+    },
+
     isBehavioral: function(path) {
       if (bids_schema) {
         return bids_schema.datatypes['beh'].some(regex => regex.exec(path))
@@ -365,7 +376,8 @@ export default {
         this.isBehavioral(path) ||
         this.isFuncBold(path) ||
         this.isPET(path) ||
-        this.isPETBlood(path)
+        this.isPETBlood(path) ||
+        this.isMicroscopy(path)
       )
     },
   },
